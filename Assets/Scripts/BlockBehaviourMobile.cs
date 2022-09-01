e53e4fc51081add87559f4977b8c86fae45b837e
shreya <shreyasethi47@gmail.com>
2022-09-01 13:30:27 +0530

    completed destop game

--- Assets/Scripts/BlockBehaviourMobile.cs
+++ Assets/Scripts/BlockBehaviourMobile.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class BlockBehaviour : MonoBehaviour
+public class BlockBehaviourMobile : MonoBehaviour
 {
     public float fallTime=1f;
     private float previousTime;
@@ -12,11 +12,13 @@ public class BlockBehaviour : MonoBehaviour
     private static int height = 20;
     public Vector3 rotationPoint;
     private static Transform[,] grid = new Transform[width, height];
+    public Button[] buttons;
     private UIManager uIManager;
+    private bool isSpeedIncrease = false;
 
     private void Start()
     {
-
+        buttons = new Button[4];
         uIManager = GameObject.FindObjectOfType<UIManager>();
         if (uIManager == null)
         {
@@ -26,55 +28,79 @@ public class BlockBehaviour : MonoBehaviour
         if (soundManager == null)
         {
             Debug.Log("sound manager not found");
-        }   
+        }
+
+        buttons[0] = GameObject.FindGameObjectWithTag("upbutton").GetComponent<Button>();
+        if (buttons[0] == null)
+        {
+            Debug.Log("Up Button not found");
+        }
+        else
+        {
+
+            buttons[0].onClick.AddListener(UpMovement);
+        }
+        
+        
+        buttons[1] = GameObject.FindGameObjectWithTag("downbutton").GetComponent<Button>();
+        if (buttons[1] == null)
+        {
+            Debug.Log("Down Button not found");
+        }
+        else
+        {
+            uIManager.GetComponent<UIManager>().IncreaseScore();
+            isSpeedIncrease = true;
+            buttons[1].onClick.AddListener(DownMovement);
+        }
+        
+        buttons[2] = GameObject.FindGameObjectWithTag("leftbutton").GetComponent<Button>();
+        if (buttons[2] == null)
+        {
+            Debug.Log("Left Button not found");
+        }
+        else
+        {
+
+            buttons[2].onClick.AddListener(LeftMovement);
+        }
+        
+        buttons[3] = GameObject.FindGameObjectWithTag("rightbutton").GetComponent<Button>();
+        if (buttons[3] == null)
+        {
+            Debug.Log("Right Button not found");
+        }
+        else
+        {
+
+            buttons[3].onClick.AddListener(RightMovement);
+        }
+  
     }
 
-    void Update()
+   
+
+    public void UpMovement()
     {
-        // for Movement of block
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
+
+        if (!isValid())
         {
-            transform.position += Vector3.left;
-            if (!isValid())
-            {
-                transform.position -= Vector3.left;
-            }
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
+        }
+    }
+
+    public void DownMovement()
+    {
+        if (Input.GetKey(KeyCode.DownArrow)){
+            isSpeedIncrease = true;
+            uIManager.GetComponent<UIManager>().IncreaseScore();
         }
 
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            transform.position += Vector3.right;
-            if (!isValid())
-            {
-                transform.position -= Vector3.right;
-            }
-        }
-
-        // rotation
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-
-            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), 90);
-
-            if (!isValid())
-            {
-                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0, 0, 1), -90);
-            }
-
-        }
-
-
-        // speed 
-        
-
-        if (Time.time - previousTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+        if (Time.time - previousTime > (isSpeedIncrease? fallTime/10 : fallTime))
         {
             previousTime = Time.time;
             transform.position += Vector3.down;
-            if (Input.GetKey(KeyCode.DownArrow))
-            {
-                uIManager.IncreaseScore();
-            }
 
             soundManager.SelectAudio(1, 0.5f);
 
@@ -102,7 +128,53 @@ public class BlockBehaviour : MonoBehaviour
                 }
 
             }
+            isSpeedIncrease = false;
         }
+
+    }
+
+    public void RightMovement()
+    {
+        transform.position += Vector3.right;
+        if (!isValid())
+        {
+            transform.position -= Vector3.right;
+        }
+    }
+
+    public void LeftMovement()
+    {
+        transform.position += Vector3.left;
+        if (!isValid())
+        {
+            transform.position -= Vector3.left;
+        }
+    }
+
+    void Update()
+    {
+        // for Movement of block
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            LeftMovement();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            RightMovement();
+        }
+
+        // rotation
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+
+            UpMovement();
+            
+        }
+
+
+        // speed 
+        DownMovement();    
     }
 
     
@@ -125,11 +197,11 @@ public class BlockBehaviour : MonoBehaviour
             var roundedY = Mathf.RoundToInt(children.position.y);
 
             
-            //if (roundedY > height - 3)
-            //{
-            //    Debug.Log("Exceed the height");
-            //    uIManager.isGameOver = true;   
-            //}
+            if (roundedY > height - 3)
+            {
+                Debug.Log("Exceed the height");
+                uIManager.isGameOver = true;   
+            }
 
             if (roundedX < 0 || roundedX >= width || roundedY < 0 || roundedY >= height)
             {
